8aa8e76e4b05b1fdee68b20a5cce3c7bb5758503
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-02-14 15:19:18 -0800

    completed task 2-4 of the mvp challenge.

--- index.js
+++ index.js
@@ -62,9 +62,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  
+  return Math.floor(Math.random()*3);
 }
+console.log(inning(4));
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,18 +83,34 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, number){
+    const result = { Home: 0, Away: 0};
+
+    for (let i=0; i<number; i++){
+
+      result.Home +=  inning();
+
+      result.Away +=  inning();
+
+    }
+    return result;
 }
+console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(callback) {
+
+  const startScore = {Home: callback(), Away: callback() };
+
+
+
+  return startScore;
 }
+console.log(getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
